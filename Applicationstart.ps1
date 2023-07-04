96c9b0cd4a353deff4d9286b4061a4624335b582
Pradee67 <109140915+Pradee67@users.noreply.github.com>
2023-07-04 20:46:12 +0530

    output added

--- Applicationstart.ps1
+++ Applicationstart.ps1
@@ -1,17 +1,22 @@
+# Stop the console application if it's already running
 # Set the application path
+
+
+Get-Process -Name "HelloApp" -ErrorAction SilentlyContinue | Stop-Process -Force
+
+# Specify the path to your console application executable
 $consoleAppPath = "C:\Hello\HelloApp"
 
-# Set the output file path
-$outputFilePath = "C:\Outputfile\Output.txt"
 
+# Specify the path for the output file
+$outputFilePath = "C:\Outputfile\Output.txt"
 # Get the current timestamp
 $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
 
-# Run the console application and capture the output with the timestamp
-$outputContent = & dotnet run --no-restore --no-build --project $consoleAppPath
+# Run the console application and redirect the output to a file
+Start-Process -FilePath "dotnet" -ArgumentList "run", "--no-restore", "--no-build", "--project", $consoleAppPath, "--", ">", $outputFilePath -NoNewWindow -Wait
 
-# Combine the output with the timestamp
-$outputContentWithTimestamp = "Console application output ($timestamp):`r`n$outputContent"
-
-# Append the output to the file
-$outputContentWithTimestamp | Out-File -FilePath $outputFilePath -Append -Encoding UTF8
+# Read the output file and display its contents
+$outputContent = Get-Content -Path $outputFilePath -Raw
+Write-Host "Console application output:"
+Write-Host $outputContent
