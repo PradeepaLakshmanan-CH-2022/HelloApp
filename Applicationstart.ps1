d8eb294f677d577c8e58ec11ad81d0b5c5c15614
Pradee67 <109140915+Pradee67@users.noreply.github.com>
2023-07-04 20:33:53 +0530

    new

--- Applicationstart.ps1
+++ Applicationstart.ps1
@@ -1,17 +1,20 @@
 # Stop the console application if it's already running
+# Set the application path
+
+
 Get-Process -Name "HelloApp" -ErrorAction SilentlyContinue | Stop-Process -Force
 
 # Specify the path to your console application executable
 $consoleAppPath = "C:\Hello\HelloApp"
 
+
 # Specify the path for the output file
 $outputFilePath = "C:\Outputfile\Output.txt"
-
 # Get the current timestamp
 $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
 
 # Run the console application and redirect the output to a file
-Start-Process -FilePath "dotnet" -ArgumentList "run", "--no-restore", "--no-build", "--project", $consoleAppPath -NoNewWindow -Wait | Out-File -FilePath $outputFilePath -Encoding UTF8
+Start-Process -FilePath "dotnet" -ArgumentList "run", "--no-restore", "--no-build", "--project", $consoleAppPath, "--", ">", $outputFilePath -NoNewWindow -Wait
 
 # Read the output file and display its contents
 $outputContent = Get-Content -Path $outputFilePath -Raw
