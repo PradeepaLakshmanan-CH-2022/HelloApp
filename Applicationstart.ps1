03d1afacaddbc34f05e49aaccb84ce2e531ddd02
Pradee67 <109140915+Pradee67@users.noreply.github.com>
2023-07-04 18:29:44 +0530

    app start

--- Applicationstart.ps1
+++ Applicationstart.ps1
@@ -1,14 +1,18 @@
 # Stop the console application if it's already running
-Stop-Process -Name "HelloApp" -ErrorAction SilentlyContinue
+# Set the application path
+
+
+Get-Process -Name "HelloApp" -ErrorAction SilentlyContinue | Stop-Process -Force
 
 # Specify the path to your console application executable
 $consoleAppPath = "C:\Hello\HelloApp"
+
 
 # Specify the path for the output file
 $outputFilePath = "C:\Outputfile\Output.txt"
 
 # Run the console application and redirect the output to a file
-Start-Process -FilePath "dotnet" -ArgumentList "run", "--no-restore", "--no-build", "--project", $consoleAppPath -NoNewWindow -Wait -RedirectStandardOutput $outputFilePath
+Start-Process -FilePath "dotnet" -ArgumentList "run", "--no-restore", "--no-build", "--project", $consoleAppPath, "--", ">", $outputFilePath -NoNewWindow -Wait
 
 # Read the output file and display its contents
 $outputContent = Get-Content -Path $outputFilePath -Raw
